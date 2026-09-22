7a04b1c8fecbcc969c83e00b0f0452db31af32d3
Miguel Boton <mboton@gharialdata.com>
2026-09-22 15:57:07 +0200

    chore: serve binaries from files.gharialdata.com

--- Package.swift
+++ Package.swift
@@ -17,17 +17,17 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Gharial",
-            url: "https://resources.gharialdata.com/sdk/ios/Gharial-1.0.0.xcframework.zip",
+            url: "https://files.gharialdata.com/sdk/ios/Gharial-1.0.0.xcframework.zip",
             checksum: "6cdaa1f01762e79dbd559c9af326352bc004933803d71aed097b6525e7bc7020"
         ),
         .binaryTarget(
             name: "WavebrookCore",
-            url: "https://resources.gharialdata.com/sdk/ios/WavebrookCore-2.0.0.xcframework.zip",
+            url: "https://files.gharialdata.com/sdk/ios/WavebrookCore-2.0.0.xcframework.zip",
             checksum: "5fb3f508736a1512c3ca384f7dbb3a575c0018bfd61d2206e37c70856384f825"
         ),
         .binaryTarget(
             name: "WavebrookAdapterRootlake",
-            url: "https://resources.gharialdata.com/sdk/ios/WavebrookAdapterRootlake-1.1.1.0.xcframework.zip",
+            url: "https://files.gharialdata.com/sdk/ios/WavebrookAdapterRootlake-1.1.1.0.xcframework.zip",
             checksum: "14ce8419dfce74623a477d2023c2d5bacb9ed2eaa4bc931d29e5d3112b240811"
         ),
         .target(
